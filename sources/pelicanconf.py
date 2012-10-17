740a33ce6a408dd34977e7826bcebf105ae2c404
Piotr Deszynski <deszynski@red-sky.pl>
2012-10-17 13:23:53 +0200

    Change into new domain

--- sources/pelicanconf.py
+++ sources/pelicanconf.py
@@ -3,7 +3,7 @@
 
 AUTHOR = u"Piotr Deszyński"
 SITENAME = u"Yet another boring programmer's blog"
-SITEURL = ''
+SITEURL = 'http://www.piotrdeszynski.com'
 
 TIMEZONE = 'Europe/Paris'
 
@@ -12,12 +12,14 @@ DEFAULT_LANG = 'en'
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
-          ('You can modify those links in your config file', '#'),)
+          )
 
 # Social widget
 SOCIAL = (('github', 'http://github.com/piteer1'),
-          ('Another social link', '#'),)
+          )
 
 DEFAULT_PAGINATION = 10
 
 GITHUB_URL = 'https://github.com/piteer1'
+
+DISQUS_SITENAME = "yetanotherboringprogrammersblog"
