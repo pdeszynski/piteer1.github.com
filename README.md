dfbb8e8c04fdce5cac9ced2e76c46b055f722d0d
Piotr Deszynski <deszynski@red-sky.pl>
2012-10-17 12:07:34 +0200

    dummy

--- README.md
+++
@@ -1,3 +0,0 @@
-#Just dummy readme file
-
-This is my blog's source. Everything's available at http://piotrdeszynski.com
--- sources/pelicanconf.py
+++ sources/pelicanconf.py
@@ -12,11 +12,10 @@ DEFAULT_LANG = 'en'
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
           ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
+SOCIAL = (('github', 'http://github.com/piteer1'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
