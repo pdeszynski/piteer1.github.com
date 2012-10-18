83e07c2afc257b3df1491c75dbf5c76b0a529900
Piotr Deszynski <deszynski@red-sky.pl>
2012-10-18 08:37:32 +0200

    Added analytics

--- sources/pelicanconf.py
+++ sources/pelicanconf.py
@@ -12,6 +12,7 @@ DEFAULT_LANG = 'en'
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
+          ('HTML5 Validator', 'http://validator.w3.org/check?uri=http%3A%2F%2Fpiotrdeszynski.com%2F'),
           )
 
 # Social widget
@@ -19,6 +20,7 @@ SOCIAL = (('github', 'http://github.com/piteer1'),
           ('linkedin.com', 'http://www.linkedin.com/pub/piotr-deszy%C5%84ski/14/762/17'),
           )
 
+GOOGLE_ANALYTICS = 'UA-35673065-1'
 DEFAULT_PAGINATION = 10
 
 GITHUB_URL = 'https://github.com/piteer1'
